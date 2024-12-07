17ce50776e6d7eccc695e1c6393856f2b5617fae
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2024-12-06 22:19:12 -0800

    code formatting

--- day1.ts
+++ day1.ts
@@ -1,45 +1,44 @@
-import fs from "fs";
+import fs from "fs"
 
-const data = fs.readFileSync("day1data.txt", "utf8");
-const lines = data.split("\n");
-const pairs = lines.map((line) => line.split("    "));
-const tuples = pairs.map((pair) => pair[0].split("   ").map(Number));
+const data = fs.readFileSync("day1data.txt", "utf8")
+const lines = data.split("\n")
+const pairs = lines.map((line) => line.split("    "))
+const tuples = pairs.map((pair) => pair[0].split("   ").map(Number))
 
 // const list1 = [3, 4, 2, 1, 3, 3];
 // const list2 = [4, 3, 5, 3, 9, 3];
 
 // Sample Data
-const list1 = tuples.map((tuple) => tuple[0]);
-const list2 = tuples.map((tuple) => tuple[1]);
-
+const list1 = tuples.map((tuple) => tuple[0])
+const list2 = tuples.map((tuple) => tuple[1])
 
 // Sort the lists smallest to largest
-const sortedList1 = list1.sort((a, b) => a - b);
-const sortedList2 = list2.sort((a, b) => a - b);
+const sortedList1 = list1.sort((a, b) => a - b)
+const sortedList2 = list2.sort((a, b) => a - b)
 
 // Get the length of the lists
-const length1 = sortedList1.length;
-const length2 = sortedList2.length;
+const length1 = sortedList1.length
+const length2 = sortedList2.length
 
 if (length1 !== length2) {
-  console.log("Lists are not the same length");
+  console.log("Lists are not the same length")
 }
 
-const distances: number[] = [];
+const distances: number[] = []
 
 for (let i = 0; i < length1; i++) {
-  const distance = Math.abs(sortedList1[i] - sortedList2[i]);
-  distances.push(distance);
+  const distance = Math.abs(sortedList1[i] - sortedList2[i])
+  distances.push(distance)
 }
 
-const totalDistance = distances.reduce((acc, curr) => acc + curr, 0);
+const totalDistance = distances.reduce((acc, curr) => acc + curr, 0)
 
-console.log("Total Distance:", totalDistance);
+console.log("Total Distance:", totalDistance)
 
 const similarityScore = sortedList1.reduce((acc, curr) => {
-  const instances = sortedList2.filter((num) => num === curr).length;
-  const increase = curr * instances;
-  return acc + increase;
-}, 0);
+  const instances = sortedList2.filter((num) => num === curr).length
+  const increase = curr * instances
+  return acc + increase
+}, 0)
 
-console.log("Similarity Score:", similarityScore);
+console.log("Similarity Score:", similarityScore)
--- day2.ts
+++ day2.ts
@@ -1,8 +1,8 @@
-import fs from "fs";
+import fs from "fs"
 
-const data = fs.readFileSync("day2data.txt", "utf8");
-const lines = data.split("\n");
-const reports = lines.map((line) => line.split(" ").map(Number));
+const data = fs.readFileSync("day2data.txt", "utf8")
+const lines = data.split("\n")
+const reports = lines.map((line) => line.split(" ").map(Number))
 
 // Sample Data
 const sampleReports = [
@@ -12,54 +12,54 @@ const sampleReports = [
   [1, 3, 2, 4, 5],
   [8, 6, 4, 4, 1],
   [1, 3, 6, 7, 9],
-];
+]
 
-let safeReports = 0;
-let safeWithDampner = 0;
+let safeReports = 0
+let safeWithDampner = 0
 
 const isIncreasing = (report: number[]) => {
   return report.every((num, index) => {
-    if (index === 0) return true;
+    if (index === 0) return true
     return (
       num > report[index - 1] &&
       num - report[index - 1] >= 1 &&
       num - report[index - 1] <= 3
-    );
-  });
-};
+    )
+  })
+}
 
 const isDecreasing = (report: number[]) => {
   return report.every((num, index) => {
-    if (index === 0) return true;
+    if (index === 0) return true
     return (
       num < report[index - 1] &&
       report[index - 1] - num >= 1 &&
       report[index - 1] - num <= 3
-    );
-  });
-};
+    )
+  })
+}
 
 const withProblemDampner = (report: number[]) => {
   for (let i = 0; i < report.length; i++) {
-    const reducedReport = report.filter((_, index) => index !== i);
+    const reducedReport = report.filter((_, index) => index !== i)
 
     if (isIncreasing(reducedReport) || isDecreasing(reducedReport)) {
-      return true;
+      return true
     }
   }
-  return false;
-};
+  return false
+}
 
 reports.map((report) => {
   if (isIncreasing(report) || isDecreasing(report)) {
-    safeReports++;
+    safeReports++
   } else if (withProblemDampner(report)) {
-    safeWithDampner++;
-  } 
+    safeWithDampner++
+  }
 
-  return safeReports;
-});
+  return safeReports
+})
 
-console.log("Safe Reports:", safeReports);
-console.log("Safe Reports with Dampner:", safeWithDampner);
-console.log("Total Safe Reports:", safeReports + safeWithDampner);
+console.log("Safe Reports:", safeReports)
+console.log("Safe Reports with Dampner:", safeWithDampner)
+console.log("Total Safe Reports:", safeReports + safeWithDampner)
