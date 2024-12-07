eacb62327dd9b1b56ac0a3917027b383401f1f80
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2024-12-06 20:42:54 -0800

    day 1 completed

--- day1.ts
+++ day1.ts
@@ -1,7 +1,17 @@
-// Sample Data
+import fs from "fs";
 
-const list1 = [3, 4, 2, 1, 3, 3];
-const list2 = [4, 3, 5, 3, 9, 3];
+const data = fs.readFileSync("day1data.txt", "utf8");
+const lines = data.split("\n");
+const pairs = lines.map((line) => line.split("    "));
+const tuples = pairs.map((pair) => pair[0].split("   ").map(Number));
+
+// const list1 = [3, 4, 2, 1, 3, 3];
+// const list2 = [4, 3, 5, 3, 9, 3];
+
+// Sample Data
+const list1 = tuples.map((tuple) => tuple[0]);
+const list2 = tuples.map((tuple) => tuple[1]);
+
 
 // Sort the lists smallest to largest
 const sortedList1 = list1.sort((a, b) => a - b);
@@ -24,4 +34,12 @@ for (let i = 0; i < length1; i++) {
 
 const totalDistance = distances.reduce((acc, curr) => acc + curr, 0);
 
-console.log(totalDistance);
+console.log("Total Distance:", totalDistance);
+
+const similarityScore = sortedList1.reduce((acc, curr) => {
+  const instances = sortedList2.filter((num) => num === curr).length;
+  const increase = curr * instances;
+  return acc + increase;
+}, 0);
+
+console.log("Similarity Score:", similarityScore);
