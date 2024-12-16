a82378b9664ca59458f0dd083ca068c6dbbc38d6
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2024-12-15 16:02:31 -0800

    wip - day 6, part 1

---
+++ day6.ts
@@ -0,0 +1,67 @@
+const sampleInput = `
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
+`
+
+const parsedInput = sampleInput
+  .trim()
+  .split("\n")
+  .map((line) => line.split(""))
+
+console.log(parsedInput)
+
+const directions = ["^", ">", "v", "<"]
+
+const relativePositions = {
+  ">": [0, 1],
+  "v": [1, 0],
+  "<": [0, -1],
+  "^": [-1, 0],
+}
+
+const turnRight = {
+  ">": "v",
+  "v": "<",
+  "<": "^",
+  "^": ">",
+}
+
+const getPosition = (input: string[][], directions: string[]) => {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+      if (directions.includes(input[i][j])) {
+        return [i, j]
+      }
+    }
+  }
+}
+
+const move = (input: string[][], direction: string, position: number[]) => {
+  const [x, y] = position
+  const [dx, dy] = relativePositions[direction as keyof typeof relativePositions]
+  const newPosition = [x + dx, y + dy]
+  const [newX, newY] = newPosition
+  if (input[newX][newY] === "#") {
+    return position
+  }
+  return newPosition
+}
+
+const markPath = (input: string[][], position: number[]) => {
+  const [x, y] = position
+  input[x][y] = "X"
+  return input
+}
+
+const startingPosition = getPosition(parsedInput, directions)
+
+console.log(startingPosition)
+
